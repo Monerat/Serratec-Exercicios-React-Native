336cc1f7d0075478a412c6cf4428749c759bfe6e
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-10-28 12:23:06 -0300

    App.tsx: create ImageBackground e Container

--- Aula 1/dreamcatcher-app/App.tsx
+++ Aula 1/dreamcatcher-app/App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, ImageBackground } from "react-native";
+
+import BgImage from "./src/assets/bg-padrao.png";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+      <ImageBackground source={BgImage} style={styles.bgImage} imageStyle={{ position: 'absolute', left: 0 }}>
+         <View style={styles.container}>
+            <Text style={{ color: "white" }}>Open up App.js to start working on your app!</Text>
+         </View>
+      </ImageBackground>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   bgImage: {
+      flex: 1,
+      paddingHorizontal: 8,
+      paddingTop: StatusBar.currentHeight,
+      paddingBottom: 4,
+
+      position: "relative",
+
+      backgroundColor: "white",
+   },
+   container: {
+      flex: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+   },
 });
