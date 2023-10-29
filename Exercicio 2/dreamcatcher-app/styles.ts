7f01aa315ba54725b7cf8137c984849dc4a5d609
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-10-29 15:54:07 -0300

    Exercicio 2/dreamcatcher-app

---
+++ Exercicio 2/dreamcatcher-app/styles.ts
@@ -0,0 +1,123 @@
+import { StatusBar, StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+   bgImage:{
+      position: "relative",
+      flex: 1,
+   },
+
+   scroll: {
+      flex: 1,
+      paddingHorizontal: 42,
+   },
+   contentContainer:{
+      minHeight: "100%",
+
+      justifyContent: "space-around",
+      paddingTop: StatusBar.currentHeight,
+      paddingBottom: 48,
+   },
+
+   sonhoContainer: {
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: 28,
+      gap: 8,
+   },
+
+   formContainer: {
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      gap: 20,
+   },
+   formInputContainer: {
+      width: "100%",
+
+      flexDirection: "column",
+      gap: 4,
+   },
+   formInputHorizontal: {
+      width: "100%",
+
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      gap: 8,
+   },
+
+   inputLabel: {
+      fontSize: 20,
+      textAlign: "left",
+      fontWeight: "700",
+      color: "white",
+   },
+   textBase: {
+      textAlign: "center",
+
+      fontSize: 16,
+      fontWeight: "400",
+      color: "white",
+   },
+   textInput: {
+      minHeight: 32,
+
+      flex: 1,
+      paddingHorizontal: 14,
+      paddingVertical: 7,
+
+      color: "white",
+
+      borderRadius: 21,
+      borderBottomWidth: 3.5,
+      borderBottomColor: "#5C5FB2",
+      backgroundColor: "#0000001A",
+
+      fontSize: 18,
+   },
+   textMultilineInput: {
+      height: 114,
+      maxHeight: 114,
+
+      paddingHorizontal: 14,
+      paddingVertical: 7,
+
+      borderRadius: 21,
+      borderBottomWidth: 3.5,
+      borderBottomColor: "#5C5FB2",
+      backgroundColor: "#0000001A",
+
+      fontSize: 18,
+      textAlignVertical: "top",
+      color: "white",
+   },
+
+   button: {
+      alignSelf: "flex-end",
+      marginTop: 28,
+
+      alignItems: "center",
+      justifyContent: "center",
+      paddingHorizontal: 42,
+      paddingVertical: 10,
+
+      borderRadius: 30,
+      backgroundColor: "#DD8EEA",
+
+      elevation: 3
+   },
+   buttonIcon: {
+      padding: 6,
+
+      borderRadius: 50,
+      backgroundColor: "#DD8EEA",
+      elevation: 3
+   },
+   buttonText: {
+      color: "white",
+      fontSize: 14,
+      fontWeight: "bold",
+      textTransform: "uppercase",
+   },
+});
