0141d633f0cc98f268a7d960cbdbb1f50b9e43e6
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-10-30 11:33:08 -0300

    components/Home: componentização #3

--- Exercicio_3/dreamcatcher-app/src/screens/Home/index.tsx
+++ Exercicio_3/dreamcatcher-app/src/screens/Home/index.tsx
@@ -1,66 +1,39 @@
-import { ScrollView, View, Image, Text } from "react-native";
+import {  View, Image, Text } from "react-native";
 
 import { Button } from "../../components/Button";
-import { FormInputIcon } from "../../components/FormInputIcon";
-import { FormMultilineInput } from "../../components/FormMultilineInput";
-import { FormSinglelineInput } from "../../components/FormSinglelineInput";
 
-import PencilIcon from "../../assets/PencilIcon.png";
-import PupilCatIcon from "../../assets/pupil-cat.png";
 import SleepingCat from "../../assets/sleeping-icon.png";
 
 import { styles } from "./styles";
 import { useState } from "react";
+import { ModalSonho } from "../../components/Modais/ModalSonhos";
 
-export const Home = () =>{
-    const [title, setTile] = useState<string | null>(null);
 
-    return(
-        <ScrollView style={styles.scroll} contentContainerStyle={styles.contentContainer}>
-            <View style={styles.sonhoContainer}>
-               <Image source={SleepingCat} style={{ tintColor: "white" }} />
-               <Text style={styles.textBase}>Ops, parece que não tem nenhum sonho aqui</Text>
-            </View>
+export interface Sonho{
+   title: string;
+   data: string;
+   descricao: string;
+   tag: string;
+};
 
-      
-            <View style={styles.formContainer}>
-               <FormSinglelineInput
-                  label="Adicione um sonho"
-                  placeholder="Digite um título"
-                  value={title}
-                  setValue={setTile}
-               />
-               <FormInputIcon
-                  label="Data:"
-                  value={title ?? new Date().toLocaleDateString()}
-                  onChangeText={setTile}
-                  iconButton={PencilIcon}
-                  iconStyle={{ height: 24, width: 24, tintColor: "white", margin:4 }}
-                  editable={false}
-                  onIconPress={()=>console.log(title, "DATA")}
-               />
-               <FormMultilineInput
-                  label="Descrição:"
-                  placeholder="Descreva seu sonho"
-                  value={title}
-                  setValue={setTile}
-               />
-               <FormInputIcon
-                  label="Tag:"
-                  placeholder="Digite uma Tag"
-                  value={title}
-                  onChangeText={setTile}
-                  iconButton={PupilCatIcon}
-                  iconStyle={{ height: 36, width: 36 }}
-                  onIconPress={()=>console.log(title, "TAG")}
-               />
+export const Home = () => {
+   const [modalAberto, setModalAberto] = useState<boolean>(false);
+   const [sonhos, setSonho] = useState<Sonho[]>([])
 
-               <View style={{alignItems: "flex-end", marginTop: 28, width: "100%" }}>
-                  <Button>
-                     <Text style={styles.buttonText}>Salvar</Text>
-                  </Button>
-               </View>
-            </View>
-         </ScrollView>
-    );
+   return (
+      <View style={styles.container}>
+         <View style={{flex: 0.5, justifyContent: "center"}}>
+            <Button onPress={()=>setModalAberto(true)}>
+               <Text style={styles.buttonText}>Adicionar Sonho</Text>
+            </Button>
+         </View>
+
+         <View style={[styles.sonhoContainer, {backgroundColor: "red"}]}>
+            <Image source={SleepingCat} style={{ tintColor: "white" }} />
+            <Text style={styles.textBase}>Ops, parece que não tem nenhum sonho aqui</Text>
+         </View>
+
+         {modalAberto && <ModalSonho modal={modalAberto} setModal={setModalAberto} setSonho={setSonho}/>}
+      </View>
+   );
 };
--- Exercicio_3/dreamcatcher-app/src/screens/Home/styles.ts
+++ Exercicio_3/dreamcatcher-app/src/screens/Home/styles.ts
@@ -1,44 +1,32 @@
 import { StatusBar, StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    scroll: {
-        flex: 1,
-        paddingHorizontal: 42,
-     },
-     contentContainer:{
-        minHeight: "100%",
-  
-        justifyContent: "space-around",
-        paddingTop: StatusBar.currentHeight,
-        paddingBottom: 48,
-     },
-  
-     sonhoContainer: {
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        paddingTop: 28,
-        gap: 8,
-     },
-  
-     formContainer: {
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: 20,
-     },
-  
-     textBase: {
-        textAlign: "center",
-  
-        fontSize: 16,
-        fontWeight: "400",
-        color: "white",
-     },
-        buttonText: {
-        color: "white",
-        fontSize: 14,
-        fontWeight: "bold",
-        textTransform: "uppercase",
-     },
+   container: {
+      flex: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "space-around",
+      gap: 28,
+   },
+   sonhoContainer: {
+      flex: 1,
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: 28,
+      gap: 8,
+   },
+   textBase: {
+      textAlign: "center",
+
+      fontSize: 16,
+      fontWeight: "400",
+      color: "white",
+   },
+   buttonText: {
+      color: "white",
+      fontSize: 14,
+      fontWeight: "bold",
+      textTransform: "uppercase",
+   },
 });
